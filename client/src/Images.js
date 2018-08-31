6bb4b1d526fb25d2c8365214377f1a3fa45062fc
funador <jesse.heaslip@gmail.com>
2018-08-30 20:54:16 -0500

    changing spinner to bowling ball and arrow function in images

--- client/src/Images.js
+++ client/src/Images.js
@@ -3,14 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 
 export default props => 
-  props.images.map(image => {
-    const id = image.public_id
-    return (
-      <div key={id} className='fadein'>
-        <div onClick={() => props.removeImage(id)} className='delete'>
-          <FontAwesomeIcon icon={faTimesCircle} size='2x' />
-        </div>
-        <img src={image.secure_url} alt='' />
+  props.images.map((image, i) => (
+    <div key={i} className='fadein'>
+      <div 
+        onClick={() => props.removeImage(image.public_id)} 
+        className='delete'
+      >
+        <FontAwesomeIcon icon={faTimesCircle} size='2x' />
       </div>
-    )
-  })
+      <img src={image.secure_url} alt='' />
+    </div>
+  ))
--- client/src/Spinner.js
+++ client/src/Spinner.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { faBowlingBall } from '@fortawesome/free-solid-svg-icons'
 
 export default () => 
   <div className='spinner fadein'>
-    <FontAwesomeIcon icon={faSpinner} size='5x' color='#3B5998' />
+    <FontAwesomeIcon icon={faBowlingBall} size='5x' color='#142930' />
   </div>
