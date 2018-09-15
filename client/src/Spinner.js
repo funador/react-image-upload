e1da3bf4bc2931fc4c70c2b2f57d1b260332bbfd
funador <jesse.heaslip@gmail.com>
2018-09-15 17:29:38 -0500

    adding moving bowling ball

--- client/src/Spinner.js
+++ client/src/Spinner.js
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBowlingBall } from '@fortawesome/free-solid-svg-icons'
 
 export default () => 
-  <div className='spinner fadein'>
-    <FontAwesomeIcon icon={faBowlingBall} size='5x' color='#1D3C4C' />
+  <div className='rolling'>
+    <div className='spinner fadein'>
+      <FontAwesomeIcon icon={faBowlingBall} size='5x' color='#1D3C4C' />
+    </div>
   </div>
